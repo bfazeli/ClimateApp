a08a01c4303c9a11a8c41f0238270fc8cf2f8aec
bijan102 <bfazeli1@gmail.com>
2018-01-30 15:38:57 -0800

    Created a Pod file and added all dependencies needed to the pod file.

--- Clima/WeatherViewController.swift
+++ Clima/WeatherViewController.swift
@@ -13,7 +13,7 @@ class WeatherViewController: UIViewController {
     
     //Constants
     let WEATHER_URL = "http://api.openweathermap.org/data/2.5/weather"
-    let APP_ID = "e72ca729af228beabd5d20e3b7749713"
+    let APP_ID = "b7f0fce4da1b96125a4397f8ddbcce39"
     
 
     //TODO: Declare instance variables here
